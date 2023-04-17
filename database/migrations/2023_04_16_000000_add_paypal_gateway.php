42a148eb3b98f9df0b3715cc45ef1a4212ff1fd5
Peter Pham <ppham@benevity.com>
2023-04-17 01:20:38 -0600

    feat(db): add paypal payment gateway

---
+++ database/migrations/2023_04_16_000000_add_paypal_gateway.php
@@ -0,0 +1,60 @@
+<?php
+
+use App\Models\Order;
+use App\Models\PaymentGateway;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Schema;
+
+class AddPayPalGateway extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $paypal = DB::table('payment_gateways')->where('provider_name', 'PayPal')->first();
+
+        if ($paypal) {
+            $paypal->update([
+                'admin_blade_template'    => 'ManageAccount.Partials.PayPal',
+                'checkout_blade_template' => 'Public.ViewEvent.Partials.PaymentPayPal'
+            ]);
+        } else {
+            DB::table('payment_gateways')->insert(
+                [
+                    'provider_name'           => 'PayPal',
+                    'provider_url'            => 'https://www.paypal.com',
+                    'is_on_site'              => 1,
+                    'can_refund'              => 0,
+                    'name'                    => 'PayPal',
+                    'default'                 => 0,
+                    'admin_blade_template'    => 'ManageAccount.Partials.PayPal',
+                    'checkout_blade_template' => 'Public.ViewEvent.Partials.PaymentPayPal'
+                ]
+            );
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        $paypal = PaymentGateway::where('name', 'PayPal')->first();
+
+        if ($paypal) {
+            // Set the Paypal gateway relationship to null to avoid errors when removing it
+            Order::where('payment_gateway_id', $paypal->id)->update(['payment_gateway_id' => null]);
+            Log::info('Removed');
+
+            $paypal->delete();
+        }
+
+    }
+}
