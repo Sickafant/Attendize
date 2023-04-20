830860fbbd5cfd037993d3901af8271a3523fc2b
Peter Pham <ppham@benevity.com>
2023-04-20 01:32:13 -0600

    feat: add rest config for paypal

--- resources/views/ManageAccount/Partials/PayPal.blade.php
+++ resources/views/ManageAccount/Partials/PayPal.blade.php
@@ -3,48 +3,23 @@
     <div class="row">
         <div class="col-md-6">
             <div class="form-group">
-                {!! Form::label('paypal[apiKey]', trans("ManageAccount.paypal_secret_key"), array('class'=>'control-label ')) !!}
-                {!! Form::text('paypal[apiKey]', $account->getGatewayConfigVal($payment_gateway['id'], 'apiKey'),[ 'class'=>'form-control'])  !!}
+                {!! Form::label('paypal[secret]', trans("ManageAccount.paypal_secret_key"), array('class'=>'control-label ')) !!}
+                {!! Form::text('paypal[secret]', $account->getGatewayConfigVal($payment_gateway['id'], 'secret'),[ 'class'=>'form-control'])  !!}
             </div>
         </div>
         <div class="col-md-6">
             <div class="form-group">
-                {!! Form::label('publishableKey', trans("ManageAccount.paypal_publishable_key"), array('class'=>'control-label ')) !!}
-                {!! Form::text('paypal[publishableKey]', $account->getGatewayConfigVal($payment_gateway['id'], 'publishableKey'),[ 'class'=>'form-control'])  !!}
+                {!! Form::label('clientId', trans("ManageAccount.paypal_client_id"), array('class'=>'control-label ')) !!}
+                {!! Form::text('paypal[clientId]', $account->getGatewayConfigVal($payment_gateway['id'], 'clientId'),[ 'class'=>'form-control'])  !!}
             </div>
         </div>
     </div>
     <div class="row">
         <div class="col-md-6">
             <div class="form-group">
-                {!! Form::label('paypal[username]', trans("ManageAccount.paypal_username"), array('class'=>'control-label ')) !!}
-                {!! Form::text('paypal[username]', $account->getGatewayConfigVal(config('attendize.payment_gateway_paypal'), 'username'),[ 'class'=>'form-control'])  !!}
-            </div>
-        </div>
-        <div class="col-md-6">
-            <div class="form-group">
-                {!! Form::label('paypal[password]', trans("ManageAccount.paypal_password"), ['class'=>'control-label ']) !!}
-                {!! Form::text('paypal[password]', $account->getGatewayConfigVal(config('attendize.payment_gateway_paypal'), 'password'),[ 'class'=>'form-control'])  !!}
+                {!! Form::label('paypal[testMode]', trans("ManageAccount.paypal_test_mode"), array('class'=>'control-label ')) !!}
+                {!! Form::text('paypal[testMode]', $account->getGatewayConfigVal(config('attendize.payment_gateway_paypal'), 'testMode'),[ 'class'=>'form-control'])  !!}
             </div>
         </div>
     </div>
-    <div class="row">
-        <div class="col-md-12">
-            <div class="form-group">
-                {!! Form::label('paypal[signature]', trans("ManageAccount.paypal_signature"), array('class'=>'control-label ')) !!}
-                {!! Form::text('paypal[signature]', $account->getGatewayConfigVal(config('attendize.payment_gateway_paypal'), 'signature'),[ 'class'=>'form-control'])  !!}
-            </div>
-        </div>
-    </div>
-        <div class="row">
-            <div class="col-md-12">
-                <div class="form-group">
-                    {!! Form::label('paypal[brandName]', trans("ManageAccount.branding_name"), array('class'=>'control-label ')) !!}
-                    {!! Form::text('paypal[brandName]', $account->getGatewayConfigVal(config('attendize.payment_gateway_paypal'), 'brandName'),[ 'class'=>'form-control'])  !!}
-                    <div class="help-block">
-                        @lang("ManageAccount.branding_name_help")
-                    </div>
-                </div>
-            </div>
-        </div>
 </section>
